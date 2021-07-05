26c2e69951fb3c5a87d6b93cfe4eacbbfde4d9d5
Erikson TF <sonyharv9@gmail.com>
2021-07-05 10:29:31 -0700

    Construir las tareas en el HTML

--- src/index.js
+++ src/index.js
@@ -2,15 +2,16 @@
 import './styles.css';
 
 import {Todo,TodoList} from './classes';
+import { crearTodoHtml } from './js/components';
 
 // import { Todo } from './classes/todo.class';
 // import { TodoList } from './classes/todo-list.class';
 
 const todoList = new TodoList();
 
-const tarea = new Todo('Aprender JavaScript');
-const tarea2 = new Todo('Comprar Celular');
-
+const tarea = new Todo('Aprender JavaScript!!!!');
 todoList.nuevoTodo(tarea);
-todoList.nuevoTodo(tarea2);
+
 console.log(todoList);
+
+crearTodoHtml(tarea);
