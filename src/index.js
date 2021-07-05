3b9145dce8829cc94bb37bad4740ee1c74951f05
Erikson TF <sonyharv9@gmail.com>
2021-07-05 09:33:48 -0700

    Add Todo-list class

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 
 import './styles.css';
 
-import { Todo } from './classes/todo.class';
+import {Todo,TodoList} from './classes';
 
+// import { Todo } from './classes/todo.class';
+// import { TodoList } from './classes/todo-list.class';
+
+const todoList = new TodoList();
 
 const tarea = new Todo('Aprender JavaScript');
-console.log(tarea);
+const tarea2 = new Todo('Comprar Celular');
+
+todoList.nuevoTodo(tarea);
+todoList.nuevoTodo(tarea2);
+console.log(todoList);
